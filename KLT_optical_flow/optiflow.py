550acab5c960b5c20ae5b89383af81486e22a189
Midou <qing1159364090@stu.xjtu.edu.cn>
2019-12-21 21:03:21 +0800

    cv2

--- KLT_optical_flow/optiflow.py
+++ KLT_optical_flow/optiflow.py
@@ -20,7 +20,7 @@ feature_params = dict( maxCorners = 500,
 
 class OpticalFlow:
     def __init__(self, video_path):#构造方法，初始化一些参数和视频路径  
-        self.track_len = 10
+        self.track_len = 5
         self.N = 10  
         self.tracks = []  # vector<vector<Point2f>>
         self.cap = cv.VideoCapture(video_path)  #按照视频帧率获取图像
@@ -42,17 +42,17 @@ class OpticalFlow:
             vis = new_frame.copy()  
             if len(self.tracks) > 0:
                 img1, img2 = self.old_gray, new_gray
-                feature = np.float32([vec[-1] for vec in self.tracks]).reshape(-1,1,2)#每个特征点追踪序列的最新值，也就是最近的图中的特征点坐标
+                feature = np.float32([vec[-1] for vec in self.tracks]).reshape(-1, 1, 2)#每个特征点追踪序列的最新值，也就是最近的图中的特征点坐标
                 p1, st, err = cv.calcOpticalFlowPyrLK(img1, img2, feature, None, **lk_params)#新特征点的位置（-1,1,2）
                 p0, st1, err1 = cv.calcOpticalFlowPyrLK(img2, img1, p1, None, **lk_params)#回溯生成跟踪角点 float
                 ## 判断正确跟踪点
                 d = abs(p0 - feature).reshape(-1,2).max(1)#输出每行的最大值，1×n
-                good = (d<1).reshape(-1, 1)#生成一列标志位
+                good = (d < 1).reshape(-1, 1)#生成一列标志位
                 new_tracks = []
                 for im, (x,y), flag in zip(self.tracks, np.float32(p1).reshape(-1,2), good):
                     if flag:
                         im.append((x,y))#时间序列上进行扩展
-                    if(len(im) > self.track_len):#对于存储过长序列的某个点,及时清除离开图像的部分
+                    if len(im) > self.track_len:#对于存储过长序列的某个点,及时清除离开图像的部分;也可以解决新增跟踪点重复的问题
                         del im[0]
                     new_tracks.append(im)##滤除丢失的跟踪点
                     cv.circle(vis, (x, y), 3, (0, 0, 255), -1)#BGR
@@ -77,12 +77,16 @@ class OpticalFlow:
                 raw_feature = cv.goodFeaturesToTrack(first_gray, maxCorners=100, qualityLevel=0.3, minDistance=7, mask = mask, blockSize=7)
                 feature = cv.cornerSubPix(first_gray, raw_feature, (11, 11), (-1, -1), (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_COUNT, 30, 0.01))
                 if feature is not None:  
-                    for x, y in np.float32(feature).reshape(-1, 2):  
-                        for x1, y1 in np.float32(vec[-1]for vec in self.tracks).reshape(-1,2):
-                            if x==x1 and y==y1:
-                                continue
+                    if len(self.tracks) > 0:
+                        print([vec[-1] for vec in self.tracks])
+                        for x, y in np.float32(feature).reshape(-1, 2):
+                            if (x, y) in [(int(vec[-1][0]), int(vec[-1][1])) for vec in self.tracks]:
+                                    continue
                             else:
                                 self.tracks.append([(x, y)])#新添追踪点   ([()])  -1,1,2
+                    else:
+                        for x, y in np.float32(feature).reshape(-1, 2):
+                            self.tracks.append([(x,y)])
  
                 ##确认之前的基准，避免丢失
             self.old_gray = new_gray
@@ -95,9 +99,12 @@ class OpticalFlow:
             
 
 def main():
-    import sys  
-    try: video_path = sys.argv[1]  
-    except: video_path = "/media/vickylzy/文件共享盘/A3_grade_one/class/computer_vision/Assignment2/100208AA_out.mp4"
+    import sys
+    # sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
+    try:
+        video_path = sys.argv[1]
+    except:
+        video_path = "/media/vickylzy/文件共享盘/A3_grade_one/class/computer_vision/Assignment/Assignment2/100208AA_out.mp4"
     OpticalFlow(video_path).flow()
     cv.destroyAllWindows()    
 
